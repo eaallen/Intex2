3fd39142de9f853878af4a46fe931e27732ff550
Michael Chadwick <mdchad@byu.edu>
2020-04-06 16:55:38 -0600

    start of info button

--- src/comps/Containers/Analyze.js
+++ src/comps/Containers/Analyze.js
@@ -14,11 +14,31 @@ function Analyze(props) {
             </div><br />
             <bs.Container>
                 <bs.Row>
+                    <bs.Col md={.5}>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="success" id="dropdown-basic">
+                            Dropdown Button
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem ac
+                                cusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventor
+                                e veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem 
+                                quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
+                                ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor s
+                                it amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labo
+                                re et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima venia
+                            </Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    </bs.Col>
+                </bs.Row><br></br>
+                <bs.Row>
                     <bs.Col>
-                        <PredictForm md='4'/>
+                        <PredictForm />
                     </bs.Col>
                     <bs.Col>
-                        <div md='8'>Results</div>
+                        <div>Results</div>
                     </bs.Col>
                 </bs.Row>
             </bs.Container>
