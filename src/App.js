abd67d18c2494f5511da3d36766ec08639e212d4
eaallen <elijah.a.allen@gmail.com>
2020-04-06 11:37:03 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button} from 'react-bootstrap'
+import Checkout from './comps/Formik'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      marital_status: '',
+      gender: '',
+      income: '',
+      children: '',
+      cars: '',
+      age: '',
+      education: '',
+      occupation: '',
+      home_owner: '',
+      commute_distance: '',
+      region: '',
+
+    }
+  }
+
+  handleChange(e){
+    e.preventDefault()
+    // let name =e.target.getAttribute('name')
+    
+    let new_state={[e.target.getAttribute('name')]: e.target.value}
+    this.setState(new_state)
+
+  }
+  submit(e){
+    e.preventDefault()
+    window.f1('yes') 
+    document.getElementById('msg').innerHTML = ''
+  }
+   //"Marital Status", "Gender", "Income", "Children", "Cars", "Age", "Education", "Occupation", "Home Owner", 
+  //  "Commute Distance", "Region" ], "Values": [ [ "Married", "Male", "10000", "0", "2", "18", "graduate", "managment", 
+  //  "yes", "0-1 miles", "Europe" 
+  render(){
+    return (
+      <div className="App">
+        <h1>
+          Welcome To Get Around CORS!
+        </h1>
+        
+          <Checkout></Checkout>
+          <Button variant="primary" onClick={e=>this.submit(e)}>
+            Get Results
+          </Button>
+       
+        
+        
+        <button>code</button>
+        
+
+        <p id="msg"></p>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/comps/Formik.js
@@ -0,0 +1,113 @@
+import React from 'react'
+import * as bs from 'react-bootstrap'
+import { Formik, Form, Field} from 'formik'
+import Spinner from 'react-bootstrap/Spinner'
+import { Container, Row, Col, Button, Jumbotron} from 'react-bootstrap';
+
+
+
+function Checkout(props) {
+    
+    return (
+      
+        <CheckoutController />
+       
+    )
+}
+export default Checkout
+
+
+const CheckoutController = props => {
+    const [getError, setError] = React.useState(null)
+    return (
+        <Formik
+            initialValues={{
+                name: 'Conrad Fox',
+                address1: '1234',
+                address2: '5678',
+                city: 'Provo',
+                // state: 'UT',
+                zipcode: '84602',
+            }}
+            validateOnChange={false}
+            validateOnBlur={false}
+            validate={values => {
+                const errors = {}
+                console.log('validating', values)
+                let arr_values = Object.keys(values)
+                arr_values.map( item =>{
+                    if(!values[item]){
+                        errors[item] = 'Required'
+                    }
+                })
+                
+                return errors
+            }}
+            onSubmit={async (values, actions) => {
+                console.log('submit')
+            }}
+        >{form => (
+            <PaymentForm form={form}/>
+        )}</Formik>
+    )
+}
+
+
+/**
+ * The form layout/html.
+ * This component needs finishing.
+ */
+const PaymentForm = props => (
+    <Form >
+        {Object.keys(props.form.initialValues).map(item=>{
+            return(
+                <Input title="Name:" name={item} type="text" disabled={props.form.isSubmitting}/>
+            )
+        })}
+           
+    </Form>
+)
+
+
+/**
+ * A form input.
+ *   props.title - the title that shows above the input box
+ *   props.type - the type of input (see React Bootstrap Form.Control)
+ *   props.placeholder - placeholder text in the input.
+ * This component is finished and doesn't need additional work.
+ */
+const Input = (props) => (
+    <Field name={props.name}>{rProps => (
+        <bs.Form.Group>
+           
+            <bs.Form.Control
+                disabled={props.disabled}
+                type={props.type}
+                placeholder={props.title}
+                {...rProps.field}
+            />
+            {rProps.meta.touched && rProps.meta.error &&
+                <div className="text-danger">{rProps.meta.error}</div>
+            }
+        </bs.Form.Group>
+    )}</Field>
+)
+
+const CARD_ELEMENT_OPTIONS = {
+    style: {
+        base:{
+            color: "#32325d",
+            fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+            fontSmoothing: "antialiased",
+            fontSize: "16px",
+            "::placeholder": {
+                color: "#aab7c4",
+            },
+        },
+        invalid: {
+            color: "#fa755a",
+            iconColor: "#fa755a",
+        },
+    }
+    
+}
