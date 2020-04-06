e9e225976a6a664c0f61c768b213b07bcab01b7f
eaallen <elijah.a.allen@gmail.com>
2020-04-06 15:54:15 -0600

    connected to dataworld

--- src/comps/Containers/Search.js
+++ src/comps/Containers/Search.js
@@ -19,7 +19,7 @@ class Search extends React.Component{
             method: 'post',
             url: 'https://api.data.world/v0/sql/eaallen/kandykane',
             data: {
-                query: "SELECT *FROM customer where customer_pk='rec0B81HP9ZnY4npw'",
+                query: "SELECT *FROM customer where customer_pk ='recD8VpCeX2PwKc34'",
                 includeTableSchema: true
             },
             headers:{
@@ -27,23 +27,24 @@ class Search extends React.Component{
                 Authorization: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwcm9kLXVzZXItY2xpZW50OmVhYWxsZW4iLCJpc3MiOiJhZ2VudDplYWFsbGVuOjo0YzBlYWQ5YS1kODE5LTQzMWMtYjVmOS0zNGEwZDE5MzRkOGQiLCJpYXQiOjE1Nzc3MTc5OTcsInJvbGUiOlsidXNlcl9hcGlfcmVhZCIsInVzZXJfYXBpX3dyaXRlIl0sImdlbmVyYWwtcHVycG9zZSI6dHJ1ZSwic2FtbCI6e319.XbV9G84LNvqN6RREjPKFlDLQrTtzUu5KVu46xDS7TOtGnMZ94h1PrNaAkQ6zT-79QOM7Ku2GrZdivguQ_o9jsw	'
             }
         });
+        console.log(resp.data)
+        
         this.setState({...this.state, data: resp.data[1]})
     
     }    
     render(){
-        console.log(this.state.data)
         return (
             <div className={this.props.className}>
                 
                     <Row>
-                        <Col md={4} className="bg-secondary">
+                        <Col md={3} className="bg-secondary">
                             search bar
                             <Button onClick={e=> this.queryDW()}>Search</Button>
                         </Col>
                         
-                        <Col md={8} className="bg-danger">
-                           {this.state.data? <>{Object.values(this.state.data).map(item=>{return(
-                            <Row>
+                        <Col md={9} className="bg-danger">
+                           {this.state.data? <>{Object.entries(this.state.data).map(item=>{ console.log(item); return(
+                            <Row key={item[0]+'key'}>
                                 <Col>{item[0]}</Col><Col>{item[1]}</Col>
                             </Row>
                            )})}</>:<></>}
